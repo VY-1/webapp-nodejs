0c6f1724458756aebe0818433f42c6c818784232
VY <veasna.youophon@live.com>
2023-10-13 13:07:59 -0700

    Update list header to contain 'ToDo List'

--- app.js
+++ app.js
@@ -131,6 +131,10 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(process.env.PORT || 3000, function() {
-  console.log("Server started on port ${process.env.PORT} || 3000");
+let port = process.env.PORT;
+if (port === null){
+  port = 3000;
+}
+app.listen(port, function() {
+  console.log("Server started on port ${port}");
 });
